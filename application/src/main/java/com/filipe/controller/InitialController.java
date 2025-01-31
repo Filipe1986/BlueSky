64a579b9797b4a1ca11643caa1305a39d38fd49a
GoncalvesF2 <filipe.goncalves@ppb.com>
2025-01-31 18:06:57 +0000

    Swagger without ui

--- application/src/main/java/com/filipe/controller/InitialController.java
+++ application/src/main/java/com/filipe/controller/InitialController.java
@@ -1,11 +1,13 @@
 package com.filipe.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InitialController {
 
+    @Operation(summary = "Hello endpoint", description = "Dummy return message")
     @GetMapping("/hello")
     public String getInitialMessage() {
         return "Hello, World!";
---
+++ integration-test/src/test/java/com/filipe/SwaggerIT.java
@@ -0,0 +1,29 @@
+package com.filipe;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(classes = BlueSkyApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SwaggerIT {
+
+    @LocalServerPort
+    private int port;
+
+    private static String BASE_URL;
+
+
+    @BeforeEach
+    public void setUp() {
+        BASE_URL = "http://localhost:" + port + "/v3/api-docs";
+    }
+
+    @Test
+    public void testHealth() {
+        RestAssured.get(BASE_URL)
+                .then()
+                .statusCode(200);
+    }
+}
